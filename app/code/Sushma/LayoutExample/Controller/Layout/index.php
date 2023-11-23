eb8e0b36d31e574f2a8482f19219afd8f1f86b88
sushma-tirumalasetti <sushma.tirumalasetti@hbwsl.com>
2023-11-23 22:20:01 +0530

    Custom layout handson and created a custom module to do

---
+++ app/code/Sushma/LayoutExample/Controller/Layout/index.php
@@ -0,0 +1,29 @@
+<?php
+declare (strict_types=1);
+
+namespace Sushma\LayoutExample\Controller\Layout;
+
+use Magento\Framework\App\ActionInterface;
+
+class Index implements ActionInterface{
+
+    /**
+     * @var \Magento\Framework\View\Result\PageFactory
+     */
+    protected $resultFactory;
+
+    /**
+     * @param \Magento\Framework\View\Result\PageFactory $resultFactory;
+     */
+
+    public function __construct(\Magento\Framework\View\Result\PageFactory $resultFactory){
+        $this->resultFactory = $resultFactory;
+    }
+
+    public function execute(){
+        $result =  $this->resultFactory->create();
+        $result->getConfig()->getTitle()->set("sushma");
+        $result->getLayout()->getBlock('page.main.title')->setPageTitle("Magento developer");
+        return $result;
+    }
+}   
