32692f7551ddf2d902779a1221bed4e7c119a0c9
sushma-tirumalasetti <sushma.tirumalasetti@hbwsl.com>
2023-12-19 00:31:06 +0530

    SUBSCRIBING TO CHANGES OF KNOCKOUTJS OBSERVABLES

--- app/code/Sushma/KnockoutJs/view/frontend/web/js/plain-view-model.js
+++ app/code/Sushma/KnockoutJs/view/frontend/web/js/plain-view-model.js
@@ -1,12 +1,17 @@
 define (['ko'],function(ko){
     'use strict';
     return function (config){
-        return {
-            title :ko.observable("this a very little title for a simple but good view model!"),
-            ccc : config,
-            getTitle: function(){
-                return this.title;
-            }
+        
+            const title  = ko.observable("this a very little title for a simple but good view model!");
+            title.subscribe(function(newValue){
+                console.log("Changed to",newValue);
+            });
+        title.subscribe(function(oldValue){
+            console.log("Will be changed from",oldValue);
+        },this,'beforeChange');
+        return{
+            title: title,
         }
+
     }
 });
