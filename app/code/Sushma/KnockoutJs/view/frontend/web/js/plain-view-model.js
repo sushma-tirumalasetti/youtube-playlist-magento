0108c04b97771446b7813e3889e0eb8e8cc12612
sushma-tirumalasetti <sushma.tirumalasetti@hbwsl.com>
2023-12-18 18:19:38 +0530

    INSTANTIATING JAVASCRIPT KNOCKOUTJS VIEW MODELS AND VIEW BINDINGS

---
+++ app/code/Sushma/KnockoutJs/view/frontend/web/js/plain-view-model.js
@@ -0,0 +1,8 @@
+define ([],function(){
+    'use strict';
+    return function (){
+        return {
+            title :"this a very little title for a simple but good view model!"
+        }
+    }
+});
